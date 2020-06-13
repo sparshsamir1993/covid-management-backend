92fbda85ac1c726d6f40f26d3ae345bc0b76f514
sparshsamir <sparshsamir1993@gmail.com>
2020-06-13 15:26:02 -0400

    first commit for capstone

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./keys/keys");
+const app = express();
+app.use(bodyParser.json());
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+const PORT = process.env.PORT || 5050;
+app.listen(PORT);
