c053e5425cd45c8a95e61c3722acfac33dc8951d
sparshsamir <sparshsamir1993@gmail.com>
2020-07-06 21:53:47 -0400

    admin hospital route patch and delete

--- routes/v1.0/admin/hospitalRoutes.js
+++ routes/v1.0/admin/hospitalRoutes.js
@@ -27,5 +27,39 @@ router.post("/", verifyToken(), async (req, res) => {
     res.sendStatus(500);
   }
 });
+router.patch("/", verifyToken(), async (req, res) => {
+  let { name, contact, detailedAddress, lat, lng, id } = req.body;
+  try {
+    const hospital = await Hospital.update(
+      {
+        name,
+        contact,
+        detailedAddress,
+        lat,
+        lng,
+      },
+      { where: { id: id } }
+    );
+    res.status(200).send(hospital);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+router.delete("/:id", verifyToken(), async (req, res) => {
+  let hospitalId = req.params.id;
+  const requestId = await Hospital.destroy({
+    where: {
+      id: hospitalId,
+    },
+  }).catch(errHandler);
+  console.log(requestId);
+  if (requestId < 1) {
+    res.sendStatus(404);
+  } else {
+    res.sendStatus(200).send(requestId);
+  }
+});
 
 module.exports = router;
