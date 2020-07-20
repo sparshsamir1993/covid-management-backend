f04b67c0e1f1cfed9b12fd5e2c71fb154fae952c
sparshsamir <sparshsamir1993@gmail.com>
2020-07-20 01:58:01 -0400

    appointment structure changed

--- routes/v1.0/hospitalAdmin/appointment.js
+++ routes/v1.0/hospitalAdmin/appointment.js
@@ -1,24 +1,28 @@
 const User = require("../../../models/user");
 const Appointment = require("../../../models/appointment");
-const Patient = require("../../../models/patient");
+// const Patient = require("../../../models/patient");
+// const User = require("../../../models/user");
+const Hospital = require("../../../models/patient");
 const verifyToken = require("../../../middlewares/verifyToken");
 const {
   APPOINTMENT_BOOKED,
 } = require("../../../constants/appointmentConstants");
 
-Appointment.belongsTo(Patient, {
-  as: "patient",
-});
-Patient.hasMany(Appointment, {
-  as: "appointments",
-});
-User.hasMany(Patient, {
-  as: "patient",
-});
-
-Patient.belongsTo(User, {
+Appointment.belongsTo(User, {
   as: "user",
 });
+User.hasMany(Appointment, {
+  as: "appointments",
+});
+
+Appointment.belongsTo(Hospital, {
+  as: "hospital",
+});
+Hospital.hasMany(Appointment, {
+  as: "appointments",
+});
+
+// Appointment.hasOne(Hospital, { as: "hospital" });
 
 var router = require("express").Router();
 
@@ -41,80 +45,36 @@ router.post("/book", verifyToken(), async (req, res, next) => {
       name,
     };
     if (!isNewUser) {
-      const currentUser = await User.findOne({
-        where: { id: userId },
-        include: [
-          {
-            model: Patient,
-            as: "patient",
-          },
-        ],
-      });
-      const userPatientHistory = currentUser.patient;
-      const currentHospitalPatientHistory = userPatientHistory.filter(
-        (history) => history.hospitalId === hospitalId
-      ).length
-        ? userPatientHistory.filter(
-            (history) => history.hospitalId === hospitalId
-          )[0]
-        : null;
-      if (currentHospitalPatientHistory) {
-        // patient already present in hospital records
-
-        let patientId = currentHospitalPatientHistory.id;
-
-        const newAppointment = await createAppointment(
-          {
-            patientId,
-            appointmentStatus: APPOINTMENT_BOOKED,
-            appointmentDateTime,
-          },
-          req,
-          res
-        );
-        res.status(200).send(newAppointment);
-      } else {
-        // user does not have record in this hospital
-        try {
-          const newPatient = await Patient.create({ userId, hospitalId });
-          const newAppointment = await createAppointment(
-            {
-              patientId: newPatient.id,
-              appointmentDateTime,
-              appointmentStatus: APPOINTMENT_BOOKED,
-            },
-            req,
-            res
-          );
-
-          res.status(200).send(newAppointment);
-        } catch (err) {
-          console.log(err);
-          res.sendStatus(500);
-        }
-      }
+      const newAppointment = await createAppointment(
+        {
+          userId,
+          hospitalId,
+          appointmentStatus: APPOINTMENT_BOOKED,
+          appointmentDateTime,
+        },
+        req,
+        res
+      );
+      res.status(200).send(newAppointment);
     } else {
       // if it is a new user
       //TODO
     }
-
-    // const userList = await User.findOne({ where: {} });
-    // console.log(userList);
-    // res.status(200).send(userList);
   } catch (err) {
     console.log(err);
   }
 });
 
 const createAppointment = async (data, req, res) => {
-  let { patientId, appointmentStatus, appointmentDateTime } = data;
-  console.log({ patientId, appointmentStatus, appointmentDateTime });
-  if (!patientId || !appointmentDateTime || !appointmentStatus) {
+  let { userId, appointmentStatus, appointmentDateTime, hospitalId } = data;
+  console.log({ userId, hospitalId, appointmentStatus, appointmentDateTime });
+  if (!userId || !hospitalId || !appointmentDateTime || !appointmentStatus) {
     res.status(500).send("Check Appointment Data");
   }
   try {
     const newAppointment = await Appointment.create({
-      patientId,
+      userId,
+      hospitalId,
       appointmentStatus,
       appointmentDateTime,
     });
