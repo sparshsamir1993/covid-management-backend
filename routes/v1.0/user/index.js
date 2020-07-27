980b5c66bd463d00b3843cc64935862a389bff58
shreyashMac <macwanshreyash@gmail.com>
2020-07-27 17:19:03 -0400

    hospital fetching for user based on location

--- routes/v1.0/user/index.js
+++ routes/v1.0/user/index.js
@@ -2,6 +2,7 @@ var router = require("express").Router();
 router.use("/signOn", require("./userRoutes"));
 router.use("/question", require("./questionRoutes"));
 router.use("/questionResponse", require("./userAnswerRoutes"));
+router.use("/hospital", require("./hospitalRoutes"));
 
 
 module.exports = router;
