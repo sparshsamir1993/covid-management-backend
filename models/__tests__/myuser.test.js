27c55a90cdc4b097d2f9b9dde350dc21e75600e8
sparshsamir <sparshsamir1993@gmail.com>
2020-08-16 16:06:05 -0400

    route mocks

--- models/__tests__/myuser.test.js
+++ models/__tests__/myuser.test.js
@@ -1,15 +1,14 @@
-
-var myuser = require('../__mocks__/user_mock');
-
+var myuser = require("../__mocks__/user_mock");
 
 describe("sequelize mock test", () => {
-
-    test("should return email", async (done) => {
-
-        await myuser.getUserEmail(2).then((email) => {
-            expect(email).toMatch('smac123@gmail.com');
-            done();
-        }).catch(done);
-
-    })
-})
+  test("should return email", async (done) => {
+    await myuser
+      .getUserEmail(2)
+      .then((email) => {
+        console.log(email);
+        expect(email).toMatch("smacs123@gmail.com");
+        done();
+      })
+      .catch(done);
+  });
+});
--- routes/v1.0/hospitalAdmin/index.js
+++ routes/v1.0/hospitalAdmin/index.js
@@ -2,5 +2,6 @@ var router = require("express").Router();
 
 router.use("/user", require("./user"));
 router.use("/appointment", require("./appointment"));
+router.use("/metrics", require("./metrics"));
 
 module.exports = router;
