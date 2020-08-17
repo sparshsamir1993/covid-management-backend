616db51f5d39180f84eb848df1be95a4cba775c4
sparshsamir <sparshsamir1993@gmail.com>
2020-08-17 18:08:45 -0400

    options

--- models/__mocks__/qansweroption_mock.js
+++ models/__mocks__/qansweroption_mock.js
@@ -1,37 +1,30 @@
-var sequelizeMock = require('sequelize-mock');
+var sequelizeMock = require("sequelize-mock");
 var dbMock = new sequelizeMock();
 
-var QAnswerMock = dbMock.define('QAnswerOptions', [{
-
+var QAnswerMock = dbMock.define("QAnswerOptions", [
+  {
     optionContent: "Yes",
     questionId: 1,
     createdAt: new Date(),
     updatedAt: new Date(),
-},
-{
+  },
+  {
     optionContent: "No",
     questionId: 1,
     createdAt: new Date(),
     updatedAt: new Date(),
-}
+  },
 ]);
 
-
 getQuestionOptions = (questionId) => {
+  const questionOptions = QAnswerMock.findOne({
+    where: { questionid: questionId },
+  }).catch((error) => {
+    console.log(error);
+  });
 
-    const questionOptions = QAnswerMock.findOne({
-        where: { questionid: questionId },
-
-    }).catch((error) => {
-        console.log(error);
-
-    })
-
-    return questionOptions;
-
-}
-
-
+  return questionOptions;
+};
 
 module.exports.getQuestionOptions = getQuestionOptions;
 module.exports.dbMock = dbMock;
--- models/__tests__/qansweroption.test.js
+++ models/__tests__/qansweroption.test.js
@@ -1,13 +1,16 @@
 var questionOptions = require("../__mocks__/qansweroption_mock");
 
 describe("sequelize mock test for Question Options", () => {
-    test("should return questionOptionData", async (done) => {
-        await questionOptions.getQuestionOptions(1).then((options) => {
-            console.log(`${options.get(0)}`);
+  test("should return questionOptionData", async (done) => {
+    await questionOptions
+      .getQuestionOptions(1)
+      .then((options) => {
+        console.log("options.get(0)[1].optionContent \n\n");
+        console.log(options.get(0)["0"].optionContent);
 
-            expect(options.get('optionContent')).toMatch("Yes");
-            done();
-        }, 40000).catch(done);
-
-    })
-})
+        expect(options.get(0)["0"].optionContent + "").toMatch("Yes");
+        done();
+      }, 40000)
+      .catch(done);
+  });
+});
